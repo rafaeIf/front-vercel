8296e7f204a9b8ead4dc7c365f97bdc496069169
rafaeIf <rafa.fernandes16052007@gmail.com>
2025-11-30 16:36:52 -0300

    instalação do tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Home from './routes/Home/index.jsx'
 import Produtos from './routes/Produtos/index.jsx'
 import EditarProdutos from './routes/EditarProdutos/index.jsx'
 import Error from './routes/Error/index.jsx'
+import './globals.css'
 
 const router = createBrowserRouter([
     {
